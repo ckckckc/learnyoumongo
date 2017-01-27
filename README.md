531d575095bf8ee050205658f3f291ffa1e3e1bd
ckckckc <refala246@gmail.com>
2017-01-27 17:44:31 +0800

    Simple find

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # learnyoumongo
 
-These are codes about learning mongo.
+## Connect
+
+Start mongod on port 27017 with data as the dbpath
+-------------------------------------------------------------------------------
+
+Create the data directory.
+
+`mkdir data`
+
+To start mongo on port 27017, run 
+
+`mongod --port 27017 --dbpath=./data.`
+
+Then, in another terminal, run npm install mongodb.
+
+Then, run learnyoumongo verify.
+
+## Search for Documents
+
+
 
 source from <a href="https://github.com/evanlucas/learnyoumongo" target="_blank">learnyoumongo</a>
---
+++ find/find.js
@@ -0,0 +1,40 @@
+var mongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var age = Number(process.argv[2]) || 18;
+
+
+mongo.connect(url, function(err, db) {
+  if (err) throw err;
+
+  db.collection('parrots')
+    .find({
+      "age": {$gt: age}
+    })
+    .toArray(function(err, documents) {
+      if (err) throw err;
+      console.log(documents);
+    });
+
+  db.close();
+});
+
+// SOLUTION
+// var mongo = require('mongodb').MongoClient
+// var age = process.argv[2]
+// 
+// var url = 'mongodb://localhost:27017/learnyoumongo'
+// 
+// mongo.connect(url, function(err, db) {
+//   if (err) throw err
+//   var parrots = db.collection('parrots')
+//   parrots.find({
+//     age: {
+//       $gt: +age
+//     }
+//   }).toArray(function(err, docs) {
+//     if (err) throw err
+//     console.log(docs)
+//     db.close()
+//   })
+// })
